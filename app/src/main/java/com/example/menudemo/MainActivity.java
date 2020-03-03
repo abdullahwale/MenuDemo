e241180a3c83c0a7265805ef62b14b4032e7a465
abdullahwale <abdullahwale@gmai.com>
2020-03-03 20:16:03 +0500

    Initial commit

---
+++ app/src/main/java/com/example/menudemo/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.menudemo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.settings) {
+
+            Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+
+        } else if (id == R.id.settings2) {
+            Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+
+        } else if (id == R.id.settings3) {
+
+            Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+
+
+        } else if (id == R.id.settings4) {
+
+            Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+
+
+        }
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
+}
